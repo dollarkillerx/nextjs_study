e2611f9fc4d05fde6a28d971f44fc0903041915d
Леони́д Ильи́ч Бре́жнев <google@gmail.com>
2024-07-06 00:06:14 +0900

    add reducer

--- demo1/src/components/home/Navigation/index.tsx
+++ demo1/src/components/home/Navigation/index.tsx
@@ -3,12 +3,14 @@
 import Menubar from "@/components/home/Navigation/Menubar";
 import Toolbar from "@/components/home/Navigation/Toolbar";
 import {useAppContext} from "@/components/AppContext";
+import ChatList from "@/components/home/Navigation/ChatList";
 
 export default function Navigation() {
     const {state: {displayNavigation}} = useAppContext()
     return (
-        <nav className={`relative h-full w-[260px] bg-gray-900 text-gray-300 p-2 ${displayNavigation ? "" : "hidden"}`}>
+        <nav className={`flex flex-col relative h-full w-[260px] bg-gray-900 text-gray-300 p-2 ${displayNavigation ? "" : "hidden"}`}>
             <Menubar></Menubar>
+            <ChatList></ChatList>
             <Toolbar></Toolbar>
         </nav>
     )
